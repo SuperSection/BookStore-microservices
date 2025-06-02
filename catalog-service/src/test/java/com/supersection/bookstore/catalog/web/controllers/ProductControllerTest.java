1f88e22038e144c7036a10edbcb1c9b421842ea7
SuperSection <soumosarkar297@gmail.com>
2025-06-02 13:23:25 +0530

    Add Integration Tests to get product with existing Code and non-existing Code

--- catalog-service/src/test/java/com/supersection/bookstore/catalog/web/controllers/ProductControllerTest.java
+++ catalog-service/src/test/java/com/supersection/bookstore/catalog/web/controllers/ProductControllerTest.java
@@ -1,11 +1,14 @@
 package com.supersection.bookstore.catalog.web.controllers;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 import com.supersection.bookstore.catalog.AbstractIntegrationTest;
+import com.supersection.bookstore.catalog.domain.Product;
 import io.restassured.http.ContentType;
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -27,5 +30,38 @@ class ProductControllerTest extends AbstractIntegrationTest {
                 .body("isLast", is(false))
                 .body("hasNext", is(true))
                 .body("hasPrevious", is(false));
+    }
+
+    @Test
+    void shouldGetProductByCode() {
+        Product product = given().contentType(ContentType.JSON)
+                .when()
+                .get("/api/products/{code}", "P100")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .extract()
+                .body()
+                .as(Product.class);
+
+        assertThat(product.code()).isEqualTo("P100");
+        assertThat(product.name()).isEqualTo("The Hunger Games");
+        assertThat(product.description()).isEqualTo("Winning will make you famous. Losing means certain death...");
+        assertThat(product.imageUrl()).isEqualTo("https://images.gr-assets.com/books/1447303603l/2767052.jpg");
+        assertThat(product.price()).isEqualTo(new BigDecimal("34.0"));
+    }
+
+    @Test
+    void shouldReturnNotFoundWhenProductCodeNotExists() {
+        String code = "invalid_product_code";
+        given().contentType(ContentType.JSON)
+                .when()
+                .get("/api/products/{code}", code)
+                .then()
+                .statusCode(404)
+                .body("status", is(404))
+                .body("title", is("Product Not Found"))
+                .body("detail", is("Product with code " + code + " not found"))
+                .body("service", is("catalog-service"));
     }
 }
