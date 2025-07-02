9602b9960decec552587c858de1fb580758d245a
Soumo Sarkar <soumosarkar297@gmail.com>
2025-07-02 22:02:31 +0530

    Implement notification service with email support and order event handling

--- notification-service/src/main/java/com/supersection/bookstore/notifications/event/OrderEventHandler.java
+++ notification-service/src/main/java/com/supersection/bookstore/notifications/event/OrderEventHandler.java
@@ -1,12 +1,76 @@
 package com.supersection.bookstore.notifications.event;
 
+import com.supersection.bookstore.notifications.domain.NotificationService;
+import com.supersection.bookstore.notifications.domain.OrderEventEntity;
+import com.supersection.bookstore.notifications.domain.OrderEventRepository;
+import com.supersection.bookstore.notifications.domain.models.OrderCancelledEvent;
 import com.supersection.bookstore.notifications.domain.models.OrderCreatedEvent;
+import com.supersection.bookstore.notifications.domain.models.OrderDeliveredEvent;
+import com.supersection.bookstore.notifications.domain.models.OrderErrorEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Component
+@Transactional
 public class OrderEventHandler {
+    private static final Logger log = LoggerFactory.getLogger(OrderEventHandler.class);
 
-    @RabbitListener(queues = "${notifications.new-notification-queue}")
-    void handleOrderCreatedEvent(OrderCreatedEvent event) {
-        System.out.println("Order Created Event: " + event);
+    private final NotificationService notificationService;
+    private final OrderEventRepository orderEventRepository;
+
+    OrderEventHandler(NotificationService notificationService, OrderEventRepository orderEventRepository) {
+        this.notificationService = notificationService;
+        this.orderEventRepository = orderEventRepository;
+    }
+
+    @RabbitListener(queues = "${notifications.new-orders-queue}")
+    public void handle(OrderCreatedEvent event) {
+        if (orderEventRepository.existsByEventId(event.eventId())) {
+            log.warn("Received duplicate OrderCreatedEvent with eventId: {}", event.eventId());
+            return;
+        }
+        log.info("Received a OrderCreatedEvent with orderNumber:{}: ", event.orderNumber());
+        notificationService.sendOrderCreatedNotification(event);
+        var orderEvent = new OrderEventEntity(event.eventId());
+        orderEventRepository.save(orderEvent);
+    }
+
+    @RabbitListener(queues = "${notifications.delivered-orders-queue}")
+    public void handle(OrderDeliveredEvent event) {
+        if (orderEventRepository.existsByEventId(event.eventId())) {
+            log.warn("Received duplicate OrderDeliveredEvent with eventId: {}", event.eventId());
+            return;
+        }
+        log.info("Received a OrderDeliveredEvent with orderNumber:{}: ", event.orderNumber());
+        notificationService.sendOrderDeliveredNotification(event);
+        var orderEvent = new OrderEventEntity(event.eventId());
+        orderEventRepository.save(orderEvent);
+    }
+
+    @RabbitListener(queues = "${notifications.cancelled-orders-queue}")
+    public void handle(OrderCancelledEvent event) {
+        if (orderEventRepository.existsByEventId(event.eventId())) {
+            log.warn("Received duplicate OrderCancelledEvent with eventId: {}", event.eventId());
+            return;
+        }
+        notificationService.sendOrderCancelledNotification(event);
+        log.info("Received a OrderCancelledEvent with orderNumber:{}: ", event.orderNumber());
+        var orderEvent = new OrderEventEntity(event.eventId());
+        orderEventRepository.save(orderEvent);
+    }
+
+    @RabbitListener(queues = "${notifications.error-orders-queue}")
+    public void handle(OrderErrorEvent event) {
+        if (orderEventRepository.existsByEventId(event.eventId())) {
+            log.warn("Received duplicate OrderErrorEvent with eventId: {}", event.eventId());
+            return;
+        }
+        log.info("Received a OrderErrorEvent with orderNumber:{}: ", event.orderNumber());
+        notificationService.sendOrderErrorEventNotification(event);
+        OrderEventEntity orderEvent = new OrderEventEntity(event.eventId());
+        orderEventRepository.save(orderEvent);
     }
 }
